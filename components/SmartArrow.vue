6527d5f40ff82db29da163f60cc9173b8197faa9
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-18 04:26:14 +0900

    Apply Prettier

--- components/SmartArrow.vue
+++ components/SmartArrow.vue
@@ -10,15 +10,31 @@ Simple Arrow
 -->
 
 <script setup lang="ts">
-import { onClickOutside } from '@vueuse/core'
-import { ref, computed, onMounted } from 'vue'
-import { makeId } from '@slidev/client/logic/utils.ts'
+import { onClickOutside } from "@vueuse/core";
+import { ref, computed, onMounted } from "vue";
+import { makeId } from "@slidev/client/logic/utils.ts";
 import { useSlideContext, onSlideEnter } from "@slidev/client";
 const props = defineProps<{
   id1?: string;
   id2?: string;
-  pos1?: "top" | "bottom" | "left" | "right" | "topleft" | "topright" | "bottomleft" | "bottomright";
-  pos2?: "top" | "bottom" | "left" | "right" | "topleft" | "topright" | "bottomleft" | "bottomright";
+  pos1?:
+    | "top"
+    | "bottom"
+    | "left"
+    | "right"
+    | "topleft"
+    | "topright"
+    | "bottomleft"
+    | "bottomright";
+  pos2?:
+    | "top"
+    | "bottom"
+    | "left"
+    | "right"
+    | "topleft"
+    | "topright"
+    | "bottomleft"
+    | "bottomright";
   x1?: number | string;
   y1?: number | string;
   x2?: number | string;
@@ -33,32 +49,32 @@ const { $scale } = useSlideContext();
 const trigger = ref<number>(0);
 const onChange = () => {
   trigger.value++;
-}
+};
 const elem1 = ref<HTMLElement | null>(null);
 const elem2 = ref<HTMLElement | null>(null);
 onMounted(() => {
   if (props.id1) {
-    elem1.value = document.getElementById(props.id1)
+    elem1.value = document.getElementById(props.id1);
     if (elem1.value) {
-      const observer = new MutationObserver(onChange)
+      const observer = new MutationObserver(onChange);
       observer.observe(elem1.value, { attributes: true });
 
-      const elem1OffsetParent = elem1.value.offsetParent
+      const elem1OffsetParent = elem1.value.offsetParent;
       if (elem1OffsetParent) {
-        const observer = new MutationObserver(onChange)
+        const observer = new MutationObserver(onChange);
         observer.observe(elem1OffsetParent, { attributes: true });
       }
     }
   }
   if (props.id2) {
-    elem2.value = document.getElementById(props.id2)
+    elem2.value = document.getElementById(props.id2);
     if (elem2.value) {
-      const observer = new MutationObserver(onChange)
+      const observer = new MutationObserver(onChange);
       observer.observe(elem2.value, { attributes: true });
 
-      const elem2OffsetParent = elem2.value.offsetParent
+      const elem2OffsetParent = elem2.value.offsetParent;
       if (elem2OffsetParent) {
-        const observer = new MutationObserver(onChange)
+        const observer = new MutationObserver(onChange);
         observer.observe(elem2OffsetParent, { attributes: true });
       }
     }
@@ -68,13 +84,15 @@ onSlideEnter(() => {
   setTimeout(() => {
     trigger.value++;
   });
-})
+});
 const point1 = computed(() => {
-  trigger.value;  // Trigger re-computation
+  trigger.value; // Trigger re-computation
   const elem1Rect = elem1.value?.getBoundingClientRect();
-  const elem1OffsetParentRect = elem1.value?.offsetParent?.getBoundingClientRect();
+  const elem1OffsetParentRect =
+    elem1.value?.offsetParent?.getBoundingClientRect();
   if (elem1Rect) {
-    let x = (elem1Rect.left - (elem1OffsetParentRect?.left ?? 0)) / $scale.value;
+    let x =
+      (elem1Rect.left - (elem1OffsetParentRect?.left ?? 0)) / $scale.value;
     let y = (elem1Rect.top - (elem1OffsetParentRect?.top ?? 0)) / $scale.value;
     const width = elem1Rect.width / $scale.value;
     const height = elem1Rect.height / $scale.value;
@@ -89,17 +107,19 @@ const point1 = computed(() => {
     } else if (!props.pos1?.includes("top")) {
       y += height / 2;
     }
-    return { x, y }
+    return { x, y };
   }
-  return { x: 0, y: 0 }
+  return { x: 0, y: 0 };
 });
 
 const point2 = computed(() => {
-  trigger.value;  // Trigger re-computation
+  trigger.value; // Trigger re-computation
   if (elem2.value) {
     const elem2Rect = elem2.value.getBoundingClientRect();
-    const elem2OffsetParentRect = elem2.value.offsetParent?.getBoundingClientRect();
-    let x = (elem2Rect.left - (elem2OffsetParentRect?.left ?? 0)) / $scale.value;
+    const elem2OffsetParentRect =
+      elem2.value.offsetParent?.getBoundingClientRect();
+    let x =
+      (elem2Rect.left - (elem2OffsetParentRect?.left ?? 0)) / $scale.value;
     let y = (elem2Rect.top - (elem2OffsetParentRect?.top ?? 0)) / $scale.value;
     const width = elem2Rect.width / $scale.value;
     const height = elem2Rect.height / $scale.value;
@@ -114,24 +134,24 @@ const point2 = computed(() => {
     } else if (!props.pos2?.includes("top")) {
       y += height / 2;
     }
-    return { x, y }
+    return { x, y };
   }
-  return { x: 0, y: 0 }
-})
+  return { x: 0, y: 0 };
+});
 
-const emit = defineEmits(['dblclick', 'clickOutside'])
+const emit = defineEmits(["dblclick", "clickOutside"]);
 
-const id = makeId()
+const id = makeId();
 
 const markerAttrs = {
-  markerUnits: 'strokeWidth',
+  markerUnits: "strokeWidth",
   markerHeight: 7,
   refY: 3.5,
-  orient: 'auto',
-}
+  orient: "auto",
+};
 
-const clickArea = ref<HTMLElement>()
-onClickOutside(clickArea, () => emit('clickOutside'))
+const clickArea = ref<HTMLElement>();
+onClickOutside(clickArea, () => emit("clickOutside"));
 </script>
 
 <template>
@@ -142,14 +162,31 @@ onClickOutside(clickArea, () => emit('clickOutside'))
   >
     <defs>
       <marker :id="id" markerWidth="10" refX="9" v-bind="markerAttrs">
-        <polygon points="0 0, 10 3.5, 0 7" :fill="color || 'currentColor'" @dblclick="emit('dblclick')" />
+        <polygon
+          points="0 0, 10 3.5, 0 7"
+          :fill="color || 'currentColor'"
+          @dblclick="emit('dblclick')"
+        />
       </marker>
-      <marker v-if="twoWay" :id="`${id}-rev`" markerWidth="20" refX="11" v-bind="markerAttrs">
-        <polygon points="20 0, 10 3.5, 20 7" :fill="color || 'currentColor'" @dblclick="emit('dblclick')" />
+      <marker
+        v-if="twoWay"
+        :id="`${id}-rev`"
+        markerWidth="20"
+        refX="11"
+        v-bind="markerAttrs"
+      >
+        <polygon
+          points="20 0, 10 3.5, 20 7"
+          :fill="color || 'currentColor'"
+          @dblclick="emit('dblclick')"
+        />
       </marker>
     </defs>
     <line
-      :x1="point1.x" :y1="point1.y" :x2="point2.x" :y2="point2.y"
+      :x1="point1.x"
+      :y1="point1.y"
+      :x2="point2.x"
+      :y2="point2.y"
       :stroke="color || 'currentColor'"
       :stroke-width="width || 2"
       :marker-end="`url(#${id})`"
@@ -158,7 +195,10 @@ onClickOutside(clickArea, () => emit('clickOutside'))
     />
     <line
       ref="clickArea"
-      :x1="point1.x" :y1="point1.y" :x2="point2.x" :y2="point2.y"
+      :x1="point1.x"
+      :y1="point1.y"
+      :x2="point2.x"
+      :y2="point2.y"
       stroke="transparent"
       stroke-linecap="round"
       :stroke-width="20"
