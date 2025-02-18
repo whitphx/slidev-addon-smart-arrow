252510283ed865affe3346a08b1af65c8fc1a845
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-18 21:37:42 +0900

    Arc (#1)
    * [WIP] Arc option
    
    * Rotate arrow heads
    
    * Delete click area
    
    * Dynamic arrow size
    
    * update
    
    * Fix
    
    * refactoring
    
    * Add arrowHeadType
    
    * Delete rough prop and always use the rough mode
    
    * Fix angle for line
    
    * Delete dbclick handler

--- components/FancyArrow.vue
+++ components/FancyArrow.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onClickOutside } from "@vueuse/core";
 import { ref } from "vue";
-import { makeId } from "@slidev/client/logic/utils.ts";
+import { slideWidth, slideHeight } from "@slidev/client";
 import { useElementPosition, type SnapPosition } from "./use-element-position";
 import { useRoughArrow } from "./use-rough-arrow";
 
@@ -14,10 +14,11 @@ const props = defineProps<{
   y1?: number | string;
   x2?: number | string;
   y2?: number | string;
-  rough?: boolean;
   width?: number | string;
   color?: string;
   twoWay?: boolean;
+  arc?: number | string;
+  arrowHeadType?: "line" | "polygon";
 }>();
 
 const point1 = props.id1
@@ -28,24 +29,16 @@ const point2 = props.id2
   : ref({ x: Number(props.x2 ?? 0), y: Number(props.y2 ?? 0) });
 
 const emit = defineEmits(["dblclick", "clickOutside"]);
-const id = makeId();
 
-const markerAttrs = {
-  markerUnits: "strokeWidth",
-  markerHeight: 7,
-  refY: 3.5,
-  orient: "auto",
-};
-
-const roughSvg = props.rough
-  ? useRoughArrow({
-      point1,
-      point2,
-      color: props.color ?? "currentColor",
-      width: Number(props.width ?? 2),
-      twoWay: props.twoWay ?? false,
-    })
-  : null;
+const roughSvg = useRoughArrow({
+  point1,
+  point2,
+  color: props.color ?? "currentColor",
+  width: Number(props.width ?? 2),
+  twoWay: props.twoWay ?? false,
+  centerPositionParam: Number(props.arc ?? 0),
+  arrowHeadType: props.arrowHeadType ?? "line",
+});
 
 const clickArea = ref<HTMLElement>();
 onClickOutside(clickArea, () => emit("clickOutside"));
@@ -55,57 +48,9 @@ onClickOutside(clickArea, () => emit("clickOutside"));
   <svg
     v-if="point1 && point2"
     class="absolute left-0 top-0"
-    :width="Math.max(point1.x, point2.x) + 50"
-    :height="Math.max(point1.y, point2.y) + 50"
+    :width="slideWidth"
+    :height="slideHeight"
   >
-    <template v-if="props.rough">
-      <g v-html="roughSvg" @dblclick="emit('dblclick')" />
-    </template>
-    <template v-else>
-      <defs>
-        <marker :id="id" markerWidth="10" refX="9" v-bind="markerAttrs">
-          <polygon
-            points="0 0, 10 3.5, 0 7"
-            :fill="color || 'currentColor'"
-            @dblclick="emit('dblclick')"
-          />
-        </marker>
-        <marker
-          v-if="twoWay"
-          :id="`${id}-rev`"
-          markerWidth="20"
-          refX="11"
-          v-bind="markerAttrs"
-        >
-          <polygon
-            points="20 0, 10 3.5, 20 7"
-            :fill="color || 'currentColor'"
-            @dblclick="emit('dblclick')"
-          />
-        </marker>
-      </defs>
-      <line
-        :x1="point1.x"
-        :y1="point1.y"
-        :x2="point2.x"
-        :y2="point2.y"
-        :stroke="color || 'currentColor'"
-        :stroke-width="width || 2"
-        :marker-end="`url(#${id})`"
-        :marker-start="twoWay ? `url(#${id}-rev)` : 'none'"
-        @dblclick="emit('dblclick')"
-      />
-    </template>
-    <line
-      ref="clickArea"
-      :x1="point1.x"
-      :y1="point1.y"
-      :x2="point2.x"
-      :y2="point2.y"
-      stroke="transparent"
-      stroke-linecap="round"
-      :stroke-width="20"
-      @dblclick="emit('dblclick')"
-    />
+    <g v-html="roughSvg" />
   </svg>
 </template>
--- components/use-rough-arrow.ts
+++ components/use-rough-arrow.ts
@@ -5,10 +5,10 @@ type RoughSVG = ReturnType<typeof roughjs.svg>;
 
 const createArrowHeadSvg = (
   rc: RoughSVG,
-  color: string,
-  width: number,
+  arrowSize: number,
+  type: "line" | "polygon",
+  options: Parameters<RoughSVG["line"]>[4],
 ): SVGGElement => {
-  const arrowSize = 20;
   const arrowAngle = Math.PI / 6; // 30 degrees
 
   const x1 = -arrowSize * Math.cos(arrowAngle);
@@ -18,17 +18,23 @@ const createArrowHeadSvg = (
 
   const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
 
-  const line1 = rc.line(x1, y1, 0, 0, {
-    stroke: color,
-    strokeWidth: width,
-  });
-  g.appendChild(line1);
-
-  const line2 = rc.line(x2, y2, 0, 0, {
-    stroke: color,
-    strokeWidth: width,
-  });
-  g.appendChild(line2);
+  if (type === "line") {
+    g.appendChild(rc.line(x1, y1, 0, 0, options));
+    g.appendChild(rc.line(x2, y2, 0, 0, options));
+  } else if (type === "polygon") {
+    g.appendChild(
+      rc.polygon(
+        [
+          [x1, y1],
+          [0, 0],
+          [x2, y2],
+        ],
+        options,
+      ),
+    );
+  } else {
+    throw new Error("Invalid arrow head type");
+  }
 
   return g;
 };
@@ -38,62 +44,190 @@ export function useRoughArrow(props: {
   point2: Ref<{ x: number; y: number } | undefined>;
   color: string;
   width: number;
+  arrowHeadType: "line" | "polygon";
   twoWay: boolean;
+  centerPositionParam: number;
 }) {
-  const { point1, point2, color, width, twoWay } = props;
+  const {
+    point1: point1Ref,
+    point2: point2Ref,
+    color,
+    width,
+    arrowHeadType,
+    twoWay,
+    centerPositionParam,
+  } = props;
+
+  const options = {
+    stroke: color,
+    strokeWidth: width,
+    fill: color,
+    fillStyle: "solid",
+  };
 
   const svg = ref<SVGSVGElement>(
     document.createElementNS("http://www.w3.org/2000/svg", "svg"),
   );
   const rc = ref(roughjs.svg(svg.value));
 
-  const arrowHead1 = ref(
-    createArrowHeadSvg(rc.value as RoughSVG, color, width),
-  );
-  const arrowHead2 = ref(
-    createArrowHeadSvg(rc.value as RoughSVG, color, width),
-  );
-
   const line = computed(() => {
-    if (!point1.value || !point2.value) {
+    if (!point1Ref.value || !point2Ref.value) {
       return null;
     }
 
-    return rc.value.line(
-      point1.value.x,
-      point1.value.y,
-      point2.value.x,
-      point2.value.y,
-      {
-        stroke: color,
-        strokeWidth: width,
-      },
+    const roughSvg = rc.value as RoughSVG;
+    const point1 = point1Ref.value;
+    const point2 = point2Ref.value;
+
+    if (point1.x === point2.x && point1.y === point2.y) {
+      return;
+    }
+
+    if (centerPositionParam === 0) {
+      // Straight line.
+      // This can be interpreted as the arc's center is at infinity.
+      const svg = roughSvg.line(
+        point1.x,
+        point1.y,
+        point2.x,
+        point2.y,
+        options,
+      );
+      const angle =
+        Math.atan2(point2.y - point1.y, point2.x - point1.x) - Math.PI / 2;
+      return {
+        svg,
+        angle1: angle,
+        angle2: angle,
+        lineLength: Math.hypot(point2.x - point1.x, point2.y - point1.y),
+      };
+    }
+
+    // Midpoint of the chord (the line segment connecting the endpoints).
+    const mid = {
+      x: (point1.x + point2.x) / 2,
+      y: (point1.y + point2.y) / 2,
+    };
+
+    // The chord vector and its length.
+    const dx = point2.x - point1.x;
+    const dy = point2.y - point1.y;
+    const chordLength = Math.hypot(dx, dy);
+
+    // Unit vector perpendicular to the chord.
+    // (Here we use (-dy, dx) for the perpendicular direction.)
+    const n = {
+      x: -dy / chordLength,
+      y: dx / chordLength,
+    };
+
+    // Offset for the arc's center from the midpoint.
+    // When |centerPositionParam| equals 1, the offset is 0 (center is at the midpoint).
+    // For other values, a smaller parameter moves the center further away (resulting in lower curvature).
+    // The formula is derived from the following simultaneous equations:
+    // R = offset + centerPositionParam * chordLength / 2 (from the condition above)
+    // R^2 = offset^2 + (chordLength / 2)^2 (Pythagorean theorem)
+    const offset =
+      ((1 - Math.pow(centerPositionParam, 2)) * chordLength) /
+      (4 * centerPositionParam);
+
+    // The arc's center is obtained by offsetting the midpoint in the direction of n.
+    const center = {
+      x: mid.x + offset * n.x,
+      y: mid.y + offset * n.y,
+    };
+
+    // Pythagorean theorem.
+    const R = Math.sqrt((chordLength / 2) ** 2 + offset ** 2);
+
+    // Angles from the center to point1 and point2.
+    const angle1 = Math.atan2(point1.y - center.y, point1.x - center.x);
+    const angle2 = Math.atan2(point2.y - center.y, point2.x - center.x);
+    let startAngle: number, endAngle: number;
+    if (centerPositionParam > 0) {
+      // Clockwise.
+      startAngle = angle1;
+      endAngle = angle2;
+    } else {
+      // Counterclockwise.
+      startAngle = angle2;
+      endAngle = angle1;
+    }
+    if (endAngle < startAngle) {
+      // roughSvg.arc expects endAngle > startAngle.
+      endAngle += 2 * Math.PI;
+    }
+
+    const D = 2 * R;
+    const svg = roughSvg.arc(
+      center.x,
+      center.y,
+      D,
+      D,
+      startAngle,
+      endAngle,
+      false,
+      options,
     );
+
+    return {
+      svg,
+      angle1,
+      angle2,
+      lineLength: R * (endAngle - startAngle),
+    };
   });
+
+  const arrowSize = computed(() => {
+    if (line.value == null) {
+      return 0;
+    }
+
+    // The arrow size is proportional to the line length.
+    // The constant factor is chosen so that the arrow size is 30 when the line length is 200.
+    return (30 * Math.log(line.value.lineLength)) / Math.log(200);
+  });
+
+  const arrowHead1 = computed(() =>
+    createArrowHeadSvg(
+      rc.value as RoughSVG,
+      arrowSize.value,
+      arrowHeadType,
+      options,
+    ),
+  );
+  const arrowHead2 = computed(() =>
+    createArrowHeadSvg(
+      rc.value as RoughSVG,
+      arrowSize.value,
+      arrowHeadType,
+      options,
+    ),
+  );
 
   return computed(() => {
     svg.value.innerHTML = "";
 
-    if (line.value == null || point1.value == null || point2.value == null) {
+    if (
+      line.value == null ||
+      point1Ref.value == null ||
+      point2Ref.value == null
+    ) {
       return null;
     }
 
-    svg.value.appendChild(line.value);
-
-    const dx = point2.value.x - point1.value.x;
-    const dy = point2.value.y - point1.value.y;
-    const angle = Math.atan2(dy, dx);
+    svg.value.appendChild(line.value.svg);
 
     arrowHead2.value.setAttribute(
       "transform",
-      `translate(${point2.value.x},${point2.value.y}) rotate(${(angle * 180) / Math.PI})`,
+      `translate(${point2Ref.value.x},${point2Ref.value.y}) rotate(${(line.value.angle2 * 180) / Math.PI + (centerPositionParam >= 0 ? 90 : -90)})`,
     );
     svg.value.appendChild(arrowHead2.value);
 
     if (twoWay) {
       arrowHead1.value.setAttribute(
         "transform",
-        `translate(${point1.value.x},${point1.value.y}) rotate(${(angle * 180) / Math.PI + 180})`,
+        `translate(${point1Ref.value.x},${point1Ref.value.y}) rotate(${(line.value.angle1 * 180) / Math.PI + (centerPositionParam >= 0 ? -90 : 90)})`,
       );
       svg.value.appendChild(arrowHead1.value);
     }
